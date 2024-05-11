55456cb7717ef16222c9619f241cd0538909811e
chouheiwa <849131492@qq.com>
2024-05-11 22:52:25 +0800

    更新运行脚本，新增自动压缩。

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ python src/train.py --config config/isic2018.yaml;
 python src/test_new.py --config config/isic2016.yaml;
 python src/test_new.py --config config/isic2017.yaml;
 python src/test_new.py --config config/isic2018.yaml;
-cd /autodl-fs/data/xboundformer/ && zip -r result.zip result;
+cd /autodl-fs/data/xboundformer/ && zip -r result.zip result && rm -rf result;
 shutdown -h now;
